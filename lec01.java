d38bef728815e7fe0f56cfd2aa02046a2bfe7579
Rohit_Saxena_Jii <mrsaxena77@gmail.com>
2026-04-14 11:30:21 +0530

    update

--- lec01.java
+++ lec01.java
@@ -12,7 +12,6 @@ public class lec01 {
         }
     }
 
-
     public static void fibo(int n) {
         int a = 0;
         int b = 1;
@@ -26,7 +25,6 @@ public class lec01 {
         }
     }
 
-
     public static void frequencyOfDig (int n, int digit) {
         int count = 0;
         while (n != 0) {
@@ -38,6 +36,27 @@ public class lec01 {
         }
         System.out.print("The Frequency of " + digit + " is : " + count);
     }
+
+    public static int pow(int n) {
+        int pwr = 1;
+        while (n != 0) {
+            n /= 10;
+            pwr *= 10;
+        }
+        return pwr;
+    }
+
+    public static void digitsInForward(int n) {
+        int pow = pow(n);
+        pow /= 10;
+        while (pow > 0) {
+            int quo = n / pow;
+            n %= pow;
+            pow /= 10;
+            System.out.println(quo);
+        }
+    }
+
 
     public static void main(String[] args) {
         frequencyOfDig(scn.nextInt(), scn.nextInt());
