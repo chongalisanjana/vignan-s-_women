64430453c63f632eaa04b278347eb9e9f6d63fa4
Rohit_Saxena_Jii <mrsaxena77@gmail.com>
2026-04-16 02:23:44 +0530

    update

--- patterns.java
+++ patterns.java
@@ -78,31 +78,7 @@ public class patterns {
             }
             System.out.println("\t");
         }
-    }
-
-    public static void numPattern1(int n) {
-        // 1
-        // 1 2
-        // 1 2 3
-        // 1 2 3 4
-        // 1 2 3 4 5
-    }
-
-    public static void numPattern2(int n) {
-        // 1
-        // 2 3
-        // 4 5 6
-        // 7 8 9 10
-        // 11 12 13 14 15
-    }
-
-    public static void numPattern3(int n) {
-        // 1
-        // 2 3 2
-        // 3 4 5 4 3
-        // 2 3 2
-        // 1
-    }
+    } 
 
     public static void hollowSquare(int n) {
         int nsp = n - 2;
@@ -165,18 +141,105 @@ public class patterns {
         }
     }
 
-    public static void plusPattern (int n) {
+    public static void plusPattern(int n) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == n/2 || j == n/2) {
+                if (i == n / 2 || j == n / 2) {
                     System.out.print("*\t");
-                }
-                else {
+                } else {
                     System.out.print("\t");
                 }
             }
             System.out.println();
         }
+    }
+
+
+    // 16/04/26
+    public static void diamondOfSpaces (int n) {
+        // *	*	*	*	*	*	*	*		
+        // *	*	*			*	*	*		
+        // *	*					*	*		
+        // *							*		
+                                            
+        // *							*		
+        // *	*					*	*		
+        // *	*	*			*	*	*		
+        // *	*	*	*	*	*	*	*
+    }
+
+    public static void diamondOfSpaces2(int n) {
+        // *	*	*	*		*	*	*	*		
+        // *	*	*				*	*	*		
+        // *	*						*	*		
+        // *								*		
+        // *	*						*	*		
+        // *	*	*				*	*	*		
+        // *	*	*	*		*	*	*	*	
+    }
+    
+    public static void sandTimer(int n) {
+        // *	*	*	*	*	*	*		
+        //     *				*		
+        //         *		*		
+        //             *			
+        //         *		*		
+        //     *			 	*		
+        // *	*	*	*	*	*	*		
+    }
+    
+    public static void arrow(int n) {
+        //  			*		
+        //              *	 *		
+        //              *	 *	 *		
+        // *	*	*	*	 *	 *	 *		
+        //              *	 *	 *		
+        //              *	 *		
+        //              *	
+    }
+
+    public static void nFactorialTillN(int n) {
+        // n = 5
+
+        // 1	
+        // 5	25	
+        // 125	625	3125	
+        // 15625	78125	390625	1953125	
+        // 9765625	48828125	244140625	1220703125	1808548329	
+    }
+
+    public static void numPattern1(int n) {
+        // 1
+        // 1 2
+        // 1 2 3
+        // 1 2 3 4
+        // 1 2 3 4 5
+    }
+
+    public static void numPattern2(int n) {
+        // 1
+        // 2 3
+        // 4 5 6
+        // 7 8 9 10
+        // 11 12 13 14 15
+    }
+
+    public static void numPattern3(int n) {
+        //     1
+        //   2 3 2
+        // 3 4 5 4 3
+        //   2 3 2
+        //     1
+    }
+
+    public static void numPattern4(int n) {
+        //                          1		
+        //                     2	3	2		
+        //                 3	4	5	4	3		
+        //             4	5	6	7	6	5	4		
+        //         5	6	7	8	9	8	7	6	5		
+        //     6	7	8	9	10	11	10	9	8	7	6		
+        // 7   8	9	10	11	12	13	12	11	10	9	8	7	
     }
 
     public static void main(String[] args) {
